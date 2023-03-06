ea6ab11562766d5188199260ba8fe9d47de9a8ce
Andreas Reußner <andreas.reussner@allgeier-it.com>
2023-03-06 10:54:36 +0100

    Spring Boot Web Backend hinzugefügt

---
+++ backend/src/main/java/eu/allgeier/tech_radar/AllgeierTechRadarApplication.java
@@ -0,0 +1,13 @@
+package eu.allgeier.tech_radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllgeierTechRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllgeierTechRadarApplication.class, args);
+	}
+
+}
