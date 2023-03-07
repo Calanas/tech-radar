e7fb851c3e02bcb8ba5196edb77034d979998c79
HermannLuft <hermann.luft@allgeier-public.eu>
2023-03-07 16:45:23 +0100

    Backend Post and Get completet

--- backend/src/main/java/eu/allgeier/tech_radar/technology/TechnologyController.java
+++ backend/src/main/java/eu/allgeier/tech_radar/technology/TechnologyController.java
@@ -1,9 +1,9 @@
 package eu.allgeier.tech_radar.technology;
 
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +18,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(path = "api/v1/")
+@RequestMapping("${application.api.version.url}/technologies")
 public class TechnologyController {
 
     // private final FirestoreTemplate firestoreTemplate;
@@ -36,9 +36,13 @@ public class TechnologyController {
     // }
 
 
-    @GetMapping("getTechnologies")
-    public Flux<Technology> getTechnologies() throws InterruptedException, ExecutionException {
-        return technologyService.getTechnologies();
+    @GetMapping
+    public Flux<Technology> filterTechnologies(
+        @RequestParam(required=false) String label,
+        @RequestParam(required=false) Integer quadrant,
+        @RequestParam(required=false) Integer ring
+    ) throws InterruptedException, ExecutionException {
+        return technologyService.filterTechnologies(label, quadrant, ring);
     }
 
     @PostMapping("saveTechnology")
@@ -50,6 +54,11 @@ public class TechnologyController {
     @DeleteMapping("deleteTechnology/{id}")
     public Mono<Technology> deleteTechnology(@PathVariable String id) {
         return technologyService.deleteTechnology(id);
+    }
+
+    @PutMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Technology> updateTechnology(@PathVariable(name = "id") Long id, @RequestBody Technology newTechnology) {
+        return technologyService.updateTechnology(id, newTechnology);
     }
 
     // @PutMapping("/updateTechnology")
