f7ffea9cfd48a1816362a49634046f8c70d99b2d
HermannLuft <hermann.luft@allgeier-public.eu>
2023-03-07 11:23:31 +0100

    Testing FirestoreRepository

---
+++ backend/src/main/java/eu/allgeier/tech_radar/technology/FirestoreTemplate.java
@@ -0,0 +1,8 @@
+package eu.allgeier.tech_radar.technology;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirestoreTemplate {
+    
+}
