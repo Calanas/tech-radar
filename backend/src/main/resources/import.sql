d4d66d8b166a88e3c33910291b1a1a462110854a
Laurentiu Andries <laurentiu.andries@allgeier-public.eu>
2023-03-08 12:09:07 +0100

    deleted import.sql only used for mocking dbs

--- backend/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO technology (label, quadrant, ring, moved) VALUES ('Quarkus', 2, 3, -1);
-INSERT INTO technology (label, quadrant, ring, moved) VALUES ('JavaScript', 4, 5, 1);
-INSERT INTO technology (label, quadrant, ring, moved) VALUES ('Java', 4, 6, 1);
-
