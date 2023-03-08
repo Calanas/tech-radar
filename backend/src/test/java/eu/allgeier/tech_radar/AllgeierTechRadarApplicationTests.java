10aa9f4e6ffe04004cdf3e9264a45410149a48b4
HermannLuft <hermann.luft@allgeier-public.eu>
2023-03-08 15:25:27 +0100

    Adding Tests

--- backend/src/test/java/eu/allgeier/tech_radar/AllgeierTechRadarApplicationTests.java
+++ backend/src/test/java/eu/allgeier/tech_radar/AllgeierTechRadarApplicationTests.java
@@ -1,9 +1,13 @@
 package eu.allgeier.tech_radar;
 
+import static org.junit.Assert.assertThat;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
@@ -16,8 +20,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
@@ -26,12 +32,15 @@ import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 import eu.allgeier.tech_radar.technology.Technology;
 import eu.allgeier.tech_radar.technology.TechnologyRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = AllgeierTechRadarApplication.class)
 @AutoConfigureMockMvc
 public class AllgeierTechRadarApplicationTests {
@@ -40,41 +49,53 @@ public class AllgeierTechRadarApplicationTests {
 	private TechnologyRepository technologyRepository;
 
 	@Test
-	public void testConnectionToDatabase() throws IOException, InterruptedException, ExecutionException {
-		FileInputStream refreshToken = new FileInputStream(
-				"C:\\Users\\Hermann Luft\\Documents\\TechRadar\\tech-radar\\backend\\src\\main\\resources\\tech-radar-379911-b814391efe1e.json");
+	public void testConnection() {
+		assert technologyRepository != null;
+	}
 
-		FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance()
-				.toBuilder()
-				.setProjectId("tech-radar-379911")
-				.setCredentials(GoogleCredentials.fromStream(refreshToken))
-				.build();
-		Firestore db = firestoreOptions.getService();
-		ApiFuture<QuerySnapshot> future = db.collection("TechRadarEntry").whereEqualTo("label", "Terraform").get();
-		List<QueryDocumentSnapshot> documents = future.get().getDocuments();
-		for (DocumentSnapshot document : documents) {
-			assert false;
+	@Test
+	public void testReadFirstTechnology() {
+		Flux<Technology> allTechnologiesStream = technologyRepository.findAll();
+		StepVerifier.create(allTechnologiesStream.log())
+				.expectNextMatches(e -> e instanceof Technology)
+				.thenCancel()
+				.verify();
+	}
+
+	@Test
+	public void testTechnologyUniqueLabels() {
+		List<String> technologyLabels = technologyRepository.findAll()
+				.map(e -> e.getLabel())
+				.collectList().block();
+		Set<String> technologyLabelsSet = new HashSet<String>(technologyLabels);
+		assert technologyLabelsSet.size() == technologyLabels.size() : "Duplicate Technologies in Database!";
+	}
+
+	@Test
+	public void testValidValues() {
+		List<Technology> technologyLabels = technologyRepository.findAll().collectList().block();
+		for (Technology technology : technologyLabels) {
+			assert technology.getQuadrant() >= 1 && technology.getQuadrant() < 5
+					: String.format("Technology %s is in quadrant %d!", technology.getLabel(),
+							technology.getQuadrant());
+			assert technology.getRing() >= 1 && technology.getRing() < 5
+					: String.format("Technology %s is in ring %d!", technology.getLabel(),
+							technology.getRing());
+			assert technology.getMoved() >= -3 && technology.getMoved() <= 3
+					: String.format("Technology %s moved by %d!", technology.getLabel(),
+							technology.getMoved());
+
 		}
-
-		// .limit(1)
-		// .get()
-		// .then((snapshot) {
-		// if (checkSnapshot.size == 0) {
-		// print("Collection Absent");
-		// }
-		// });;
-
-		// ApiFuture<WriteResult> future =
-		// db.collection("citiTechRadarEntryes").document("LA").set(docData);
-
 	}
 
 	@Test
 	public void findTechnologyById_ifPresent() {
 		Technology testTechnology = new Technology("TestTechnology", 1, 0, 1);
-		technologyRepository.save(testTechnology).subscribe();
+		StringBuilder sb = new StringBuilder();
+		String resultingId = technologyRepository.save(testTechnology).block().getId(); // .subscribe(e ->
+																						// sb.append(e.getId()));
 		StepVerifier.create(technologyRepository
-				.findByLabel(testTechnology.getLabel()).log())
+				.findById(resultingId).log())
 				.expectNext(testTechnology)
 				.verifyComplete();
 		technologyRepository.deleteById(testTechnology.getId()).subscribe();
