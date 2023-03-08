444b6a42c246a2e9aad81f15f7131893967aacc3
Laurentiu Andries <laurentiu.andries@allgeier-public.eu>
2023-03-08 12:14:13 +0100

    reverted changes made on frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { TechnologyComponent } from './technology/technology.component';
 
-const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'technologies', component: TechnologyComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
