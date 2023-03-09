e57265b621eaadadf14cc5726f9ed1631f120a83
HermannLuft <hermann.luft@allgeier-public.eu>
2023-03-09 10:35:24 +0100

    Cleaning 2

--- backend/src/test/java/eu/allgeier/tech_radar/technology/TechnologyDatabaseTest.java
+++ backend/src/test/java/eu/allgeier/tech_radar/technology/TechnologyDatabaseTest.java
@@ -1,16 +1,18 @@
 package eu.allgeier.tech_radar.technology;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,22 +22,21 @@ import reactor.test.StepVerifier;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = AllgeierTechRadarApplication.class)
-public class TechnologyDatabase {
+public class TechnologyDatabaseTest {
 
 	@Autowired
 	private TechnologyRepository technologyRepository;
 
-	private Technology testTechnology;
+	private static Technology testTechnology;
 
-	@Before
-	public void init() {
-		this.testTechnology = new Technology("TestTechnology", 1, 3, 1);
+	@BeforeAll
+	static void init() {
+		testTechnology = new Technology("TestTechnology", 1, 3, 1);
 	}
-
 
 	@Test
 	public void databaseConnection() {
-		assert technologyRepository != null;
+		assertNotNull(technologyRepository);
 	}
 
 	@Test
@@ -53,46 +54,45 @@ public class TechnologyDatabase {
 				.map(e -> e.getLabel())
 				.collectList().block();
 		Set<String> technologyLabelsSet = new HashSet<String>(technologyLabels);
-		assert technologyLabelsSet.size() == technologyLabels.size() : "Duplicate Technologies in Database!";
+		assertEquals(technologyLabelsSet.size(), technologyLabels.size(), "Duplicate Technologies in Database!");
 	}
 
 	@Test
 	public void allTechnologies_ShouldHaveValidValues() {
 		List<Technology> technologyLabels = technologyRepository.findAll().collectList().block();
 		for (Technology technology : technologyLabels) {
-			assert technology.getQuadrant() >= 1 && technology.getQuadrant() < 5
-					: String.format("Technology %s is in quadrant %d!", technology.getLabel(),
-							technology.getQuadrant());
-			assert technology.getRing() >= 1 && technology.getRing() < 5
-					: String.format("Technology %s is in ring %d!", technology.getLabel(),
-							technology.getRing());
-			assert technology.getMoved() >= -3 && technology.getMoved() <= 3
-					: String.format("Technology %s moved by %d!", technology.getLabel(),
-							technology.getMoved());
+			assertTrue(technology.getQuadrant() >= 1 && technology.getQuadrant() < 5,
+					String.format("Technology %s is in quadrant %d!", technology.getLabel(),
+							technology.getQuadrant()));
+			assertTrue(technology.getRing() >= 1 && technology.getRing() < 5,
+					String.format("Technology %s is in ring %d!", technology.getLabel(),
+							technology.getRing()));
+			assertTrue(technology.getMoved() >= -3 && technology.getMoved() <= 3,
+					String.format("Technology %s moved by %d!", technology.getLabel(),
+							technology.getMoved()));
 
 		}
 	}
 
 	@Test
 	public void writeRead_ShouldHaveAccess() {
-		String resultingId = technologyRepository.save(this.testTechnology).block().getId(); // .subscribe(e ->
-		
+		String resultingId = technologyRepository.save(testTechnology).block().getId();
+
 		try {
-		StepVerifier.create(technologyRepository
-				.findById(resultingId).log())
-				.expectNext(this.testTechnology)
-				.verifyComplete();
+			StepVerifier.create(technologyRepository
+					.findById(resultingId).log())
+					.expectNext(testTechnology)
+					.verifyComplete();
 		} finally {
-			technologyRepository.deleteById(this.testTechnology.getId()).subscribe();
+			technologyRepository.deleteById(testTechnology.getId()).subscribe();
 		}
 	}
 
 	@Test
 	public void updateTechnology_ShouldUpdateInDatabase() {
-		String resultingId = technologyRepository.save(this.testTechnology).block().getId(); // .subscribe(e ->
+		String resultingId = technologyRepository.save(testTechnology).block().getId(); 
 
-		try
-		{
+		try {
 			StepVerifier.create(technologyRepository
 					.findByLabel("TestTechnology").log())
 					.expectNextMatches(t -> t.getLabel().equals("TestTechnology"))
@@ -101,19 +101,16 @@ public class TechnologyDatabase {
 			this.technologyRepository
 					.findById(resultingId)
 					.flatMap(p -> {
-						p.setLabel(this.testTechnology.getLabel());
-						p.setMoved(this.testTechnology.getMoved());
-						p.setRing(this.testTechnology.getRing());
-						p.setQuadrant(this.testTechnology.getQuadrant());
+						p.setLabel(testTechnology.getLabel());
+						p.setMoved(testTechnology.getMoved());
+						p.setRing(testTechnology.getRing());
+						p.setQuadrant(testTechnology.getQuadrant());
 						return this.technologyRepository.save(p);
 					});
-		}
-		finally {
+		} finally {
 			technologyRepository.deleteById(resultingId).subscribe();
 		}
-		
+
 	}
-
-
 
 }
