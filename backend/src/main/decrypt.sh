60d76ef58010284ee87ff1cb4318354727690b76
Laurentiu Andries <laurentiu.andries@allgeier-public.eu>
2023-03-13 13:02:52 +0100

    argument for decrypt script from env

--- backend/src/main/decrypt.sh
+++ backend/src/main/decrypt.sh
@@ -4,5 +4,5 @@
 CREDS_PATH="./resources"
 ENCRYPTED=$CREDS_PATH/"$(basename $CREDS_PATH/*.gpg)"
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" \
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
 --output $CREDS_PATH/$(basename $ENCRYPTED .gpg) $ENCRYPTED
